1c556796efb8cb95c2fb6eeb203bb3b38c6d3ff0
Eun Sung Allen Kim <siiilver9@gmail.com>
2023-04-01 18:55:18 +0900

    Update Form1.cs

--- src/c#/Form1.cs
+++ src/c#/Form1.cs
@@ -1885,34 +1885,7 @@ namespace KB_Data_V2
                     //        //---------------↓ 정렬 ↓---------------┐
                     //        //GridMaster.CenterAlign( dgv );
                     //        GridMaster.LeftAlign(dgv);
-                    //        //GridMaster.Align( dgv , 0 , DataGridViewContentAlignment.MiddleLeft );//단일 Column 정렬
-                    //        //---------------↑ 정렬 ↑---------------┘
-
-                    //        //---------------↓ 설정 ↓---------------┐
-                    //        dgv.ReadOnly = true;//읽기전용
-                    //        GridMaster.DisableSortColumn(dgv);//오름차순 내림차순 정렬 막기
-                    //        //dgv.Columns[ 0 ].ReadOnly = true;//읽기전용
-                    //        //dgv.AllowUserToResizeColumns = false;//컬럼폭 수정불가
-
-                    //        //dgv.Columns[ 1 ].DefaultCellStyle.Format = "yyyy-MM-dd HH:mm:ss";//표시형식
-                    //        dgv.ColumnHeadersVisible = false;//컬럼헤더 가리기                        
-                    //        //dgv.DefaultCellStyle.WrapMode = DataGridViewTriState.True;//스페이스 시 줄바꿈
-                    //        //dgv.DefaultCellStyle.BackColor = Color.Black;//색반전
-                    //        //dgv.DefaultCellStyle.ForeColor = Color.White;//색반전
-
-                    //        //---------------↑ 설정 ↑---------------┘
-
-
-
-                    //    }
-                    //    catch (Exception)
-                    //    {
-
-                    //    }
-
-                    //    break;
-                    //case "dgvES3":
-
+                    //        //GridMaster.Align( dgv , 0 , DataGridViewContentA
                     //    try
                     //    {
                     //        //---------------↓ 기본 ↓---------------┐
