f466c18ad316bb39fc035b18ebe7b74c9303053a
Eun Sung Allen Kim <siiilver9@gmail.com>
2023-05-30 04:24:16 +0900

    Create Program.cs
    Add code for program

---
+++ src/c#/PC2/Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KB_Data
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            bool bNew;
+            Mutex mutex = new Mutex(true, "Kabul", out bNew);
+            if (bNew)
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+
+            }
+        }
+    }
+}
